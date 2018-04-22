681ca75fdbcfe7f518817175bf71134bd85d3a68
Gergő Takács <spiroka01@gmail.com>
2018-04-22 22:09:18 +0200

    Changed the players type to KinematicBody2D

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,17 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-enum STATES {IDLE, JUMPING, CHARGING_JUMP, LAUNCHING, KITING, DASHING, WALKING}
+enum STATES {IDLE, JUMPING, CHARGING_JUMP, KITING, DASHING, WALKING}
 enum ACTIONS {LEFT_PRESSED, RIGHT_PRESSED, BOTH_PRESSED, LEFT_DOUBLE_TAPPED, RIGHT_DOUBLE_TAPPED, LEFT_HELD, RIGHT_HELD}
 
-const GRAVITY_DEFAULT = 10
-const GRAVITY_KITING = 1
+const GRAVITY_DEFAULT = 1500
+const GRAVITY_KITING = 300
 const DASH_DURATION = 0.2
 const DOUBLE_TAP_DELAY = 0.17
+const DASH_FORCE = 700
+const HORIZONTAL_JUMP_FORCE = 200
+const VERTICAL_JUMP_FORCE = -700
+const CHARGED_JUMP_FORCE = -800
+const WALK_FORCE = 300
 
 onready var _start_pos = get_node('../StartPosition')
 onready var _animation_player = get_node('AnimationPlayer')
@@ -18,15 +23,23 @@ var _time_since_last_press = 0
 var _dash_time = 0
 var _waiting_for_double_tap = false
 var _already_air_dashed = false
+var _velocity = Vector2()
+var _gravity_scale = GRAVITY_DEFAULT
+var _sleeping = false
 
 func ready():
 	_animation_player.play('idle')
 
 
-func _integrate_forces(s):
-	var action = _resolve_action(s.step)
+func _physics_process(delta):
+	if _sleeping:
+		return
 
-	_resolve_state(action, s)
+	var action = _resolve_action(delta)
+
+	_resolve_state(action, delta)
+	_velocity.y += delta * _gravity_scale
+	_velocity = move_and_slide(_velocity, Vector2(0, -1))
 
 
 func _resolve_action(delta):
@@ -72,70 +85,64 @@ func _resolve_action(delta):
 	return -1
 
 
-func _resolve_state(action, body_state):
-	var on_floor = _is_on_floor(body_state)
-	
+func _resolve_state(action, delta):
 	match _state:
 		IDLE:
 			if action == BOTH_PRESSED:
 				_state = CHARGING_JUMP
 			elif action == LEFT_DOUBLE_TAPPED or action == RIGHT_DOUBLE_TAPPED:
 				var direction = -1 if action == LEFT_DOUBLE_TAPPED else 1
-				set_axis_velocity(Vector2(direction * 600, 0))
+				_velocity = Vector2(direction * DASH_FORCE, 0)
 				_state = DASHING
 			elif action == LEFT_PRESSED or action == RIGHT_PRESSED:
 				var direction = -1 if action == LEFT_PRESSED else 1
-				set_axis_velocity(Vector2(direction * 100, -400))
-				_state = LAUNCHING
+				_velocity = Vector2(direction * HORIZONTAL_JUMP_FORCE, VERTICAL_JUMP_FORCE)
+				_state = JUMPING
 				_animation_player.play('launch')
 				_animation_player.queue('idle')
-				on_floor = false
 			elif action == LEFT_HELD or action == RIGHT_HELD:
 				_state = WALKING
 		JUMPING:
-			if on_floor:
+			if is_on_floor():
 				_state = IDLE
 				_already_air_dashed = false
-			elif action == BOTH_PRESSED and body_state.linear_velocity.y >= 0:
-				gravity_scale = GRAVITY_KITING
+				_velocity.x = 0
+			elif action == BOTH_PRESSED and _velocity.y >= 0:
+				_gravity_scale = GRAVITY_KITING
 				_state = KITING
 				_animation_player.play('kite')
 			elif (action == LEFT_DOUBLE_TAPPED or action == RIGHT_DOUBLE_TAPPED) and not _already_air_dashed:
 				var direction = -1 if action == LEFT_DOUBLE_TAPPED else 1
-				set_axis_velocity(Vector2(direction * 400, -100))
+				_velocity += Vector2(direction * DASH_FORCE, -100)
 				_already_air_dashed = true
 		CHARGING_JUMP:
-			if not _both_pressed() and on_floor:
-				set_axis_velocity(Vector2(0, -600))
-				_state = LAUNCHING
+			if not _both_pressed() and is_on_floor():
+				_velocity = Vector2(0, CHARGED_JUMP_FORCE)
+				_state = JUMPING
 				_animation_player.play('launch')
 				_animation_player.queue('idle')
-				on_floor = false
-		LAUNCHING:
-			if not on_floor:
-				_state = JUMPING
 		KITING:
-			if on_floor:
-				gravity_scale = GRAVITY_DEFAULT
+			if is_on_floor():
+				_gravity_scale = GRAVITY_DEFAULT
+				_velocity.x = 0
 				_state = IDLE
 				_animation_player.play('idle')
 			elif not _both_pressed():
-				gravity_scale = GRAVITY_DEFAULT
+				_gravity_scale = GRAVITY_DEFAULT
 				_state = JUMPING
 				_animation_player.play('idle')
 		DASHING:
-			_dash_time += body_state.step
+			_dash_time += delta
 			if _dash_time >= DASH_DURATION and not Input.is_action_pressed(_button_pressed_last):
 				_state = IDLE
 				_dash_time = 0
+				_velocity.x = 0
 		WALKING:
 			if action == LEFT_HELD or action == RIGHT_HELD:
 				var direction = -1 if action == LEFT_HELD else 1
-				friction = 0
-				linear_velocity.x = direction * 150
+				_velocity.x = direction * WALK_FORCE
 			else:
-				friction = 1
-				linear_velocity.x = 0
+				_velocity.x = 0
 				_state = IDLE
 
 
@@ -160,15 +167,15 @@ func _is_on_floor(s):
 
 
 func _on_Game_over():
-	sleeping = true
+	_sleeping = true
 
 
 func _on_Game_win():
-	sleeping = true
+	_sleeping = true
 
 
 func _on_Game_started():
 	global_position.x = _start_pos.position.x
 	global_position.y = _start_pos.position.y
-	gravity_scale = GRAVITY_DEFAULT
-	sleeping = false
+	_gravity_scale = GRAVITY_DEFAULT
+	_sleeping = false
